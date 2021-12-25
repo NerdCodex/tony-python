9bb8650851b0826d896096e47e3e14b22b57e92d
Subbu Rakesh S.B <81899310+programmingrakesh@users.noreply.github.com>
2021-12-25 14:04:17 +0530

    Update Old_main_1.py

--- Old_lexer_class/Lexer_1/Old_main_1.py
+++ Old_lexer_class/Lexer_1/Old_main_1.py
@@ -3,7 +3,7 @@ from Lexer import Compiler
 
 
 def run():
-    file = open("new.io", 'r').read()
+    file = open("new.tony", 'r').read()
     runner = lexer(data=file)
     runner.tokenizer()
     runner.Parser()
